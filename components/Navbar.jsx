efd89277ab63ef54190f7a383896ce4e03b852b1
Louai_Dourov <boumedienelouai2@gmail.com>
2023-02-06 14:52:37 -0800

    navigation bar | done

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-// import Link from 'next/link';
 import Image from 'next/image';
 
 import { Link, animateScroll as scroll } from 'react-scroll';
@@ -8,27 +7,81 @@ import logo from '../assets/images/logo.svg';
 import menu from '../assets/images/menu.svg';
 import close from '../assets/images/close.svg';
 
-const Navbar = () => {
-  const [menuOpen, setMenuOpen] = useState(true);
+const Navbar = ({overlay}) => {
+  const [menuOpen, setMenuOpen] = useState(false);
 
   const toggleMenu = () => setMenuOpen(!menuOpen);
 
   return (
-    <nav className='fixed top-0 z-10 w-full '>
+    <nav className={`fixed top-0 z-10 w-full bg-black duration-300 ${overlay ? 'bg-opacity-70' : 'bg-opacity-0'}`}>
       <div className='inner-wrapper  flex justify-between items-center'>
         <Image src={logo} alt='The Logo' />
 
         <Image
           src={menuOpen ? close : menu}
           alt='Menu Icon'
-          className='z-10 lg:hidden cursor-pointer'
+          className='z-50 lg:hidden cursor-pointer'
           onClick={toggleMenu}
         />
 
         <ul
-          className={`absolute right-1 top-2 pt-10 px-16 pb-4 flex flex-col rounded-xl bg-black justify-center items-center gap-x-4 duration-300  lg:relative lg:flex-row lg:bg-transparent lg:p-0 lg:right-0 lg:top-0 ${
-            menuOpen ? 'flex' : 'hidden'
-          }`}
+          className={`absolute  pt-10 px-16 pb-4 flex flex-col rounded-xl bg-black justify-center items-center duration-300 ${
+            menuOpen ? 'opacity-100 right-2 top-2' : 'opacity-0 right-[-50%] top-[-50%]'
+          } lg:hidden`}
+        >
+          <li>
+          <Link
+              className='nav-link'
+              activeClass='active'
+              to='home'
+              duration={300}
+              spy={true}
+              smooth={true}
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              className='nav-link'
+              activeClass='active'
+              to='contact'
+              duration={300}
+              spy={true}
+              smooth={true}
+            >
+              Contact
+            </Link>
+          </li>
+          <li>
+          <Link
+              className='nav-link'
+              activeClass='active'
+              to='about'
+              duration={300}
+              spy={true}
+              smooth={true}
+            >
+              About
+            </Link>
+          </li>
+          <li>
+          <Link
+              className='nav-link'
+              activeClass='active'
+              to='products'
+              duration={300}
+              spy={true}
+              smooth={true}
+            >
+              Products
+            </Link>
+          </li>
+        </ul>
+
+
+        <ul
+          className={`left-1/2 translate-x-[-65%] top-0 aboslute hidden justify-center items-center gap-x-6 duration-300 lg:flex`}
         >
           <li>
           <Link
--- pages/index.jsx
+++ pages/index.jsx
@@ -2,10 +2,38 @@ import Navbar from '../components/Navbar'
 import Hero from '../sections/HeroSection'
 import Contact from '../sections/ContactSection'
 
+import { useEffect, useState } from 'react'
+
+
 export default function Home() {
+
+  const [overlay, setOverlay] = useState(true)
+
+
+  useEffect(() => {
+
+    let listner = window.addEventListener('scroll', (e) => {
+
+      console.log(window.scrollY)
+
+      if (window.scrollY > 130) {
+        setOverlay(true)
+      }
+      else {
+        setOverlay(false)
+      }
+
+    })
+
+    return () => {
+      window.removeEventListener('scroll', listner)
+    }
+
+  }, [])
+
   return (
     <main className='text-center'>
-      <Navbar />
+      <Navbar overlay={overlay}/>
       <Hero />
       <Contact />
 
